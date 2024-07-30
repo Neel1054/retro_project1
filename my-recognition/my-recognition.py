2c44417aa0f7aeb112ceab054434621088f1f2b3
Neel1054 <2020nupatel@gmail.com>
2024-07-30 10:15:14 -0700

    classification exercise

---
+++ my-recognition/my-recognition.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import jetson_inference
+import jetson_utils
+import argparse
+#recogninzing and loading models+argparse parses the command line
+
+#parses the image file and selects a network, parses=decipher
+#defines expected command line arguement and parses them when script runs
+parser=argparse.ArgumentParser()
+#filename that is passed into command line
+parser.add_argument("filename", type = str, help = "filename of the image to proceed")
+#adds network going to be used
+parser.add_argument("--network", type=str, default="googlenet", help = "model to use where googlenet is the default")
+#takes in arguements from command line and convert them into object we named opt
+opt = parser.parse_args()
+
+#loads image from disk
+img = jetson_utils.loadImage(opt.filename)
+
+#load recognition network from command line
+net = jetson_inference.imageNet(opt.network)
+
+#class_idx -> reresents the index of the predicted class
+#confidence is how sure the model is that it is predicting correctly
+class_idx, confidence=net.Classify(img)
+
+class_desc=net.GetClassDesc(class_idx)
+
+print("image is recognized as "+str(class_desc)+" (class# + "+str(class_idx)+") with "+str(confidence+100)+"%")
